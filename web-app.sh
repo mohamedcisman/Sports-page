56d9bbbad76f80e32228432c719c933b20a1f521
Ubuntu <ubuntu@ip-172-31-30-218.ec2.internal>
2024-12-03 20:24:03 +0000

    Updated files

--- web-app.sh
+++ web-app.sh
@@ -1,15 +1,15 @@
 #!/bin/bash 
 
-mkdir tempdir
-mkdir tempdir/templates
-mkdir tempdir/static
+mkdir -p tempdir
+mkdir -p tempdir/templates
+mkdir -p tempdir/static
 
 cp app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
 echo "FROM python" >> tempdir/Dockerfile
-echo "RUN pip install flask" >> tempdir/Docketfile
+echo "RUN pip install flask" >> tempdir/Dockerfile
 echo "COPY ./static /webapp/static/" >> tempdir/Dockerfile
 echo "COPY ./templates/ webapp/templates/" >> tempdir/Dockerfile
 echo "COPY app.py /webapp/" >> tempdir/Dockerfile
@@ -17,8 +17,8 @@ echo "EXPOSE 5000" >> tempdir/Dockerfile
 echo "CMD python /webapp/app.py" >> tempdir/Dockerfile
 
 cd tempdir 
-sudo docker build -t webapp .
-sudo docker run -t -d -p 5000:5000 --name samplerunning webapp
-sudo docker ps -a
+docker build -t webapp .
+docker run -t -d -p 5000:5000 --name testrun webapp
+docker ps -a
 
 
